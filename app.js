af2fdee94799452bea8751ee51679ac27a3c53e0
Shubham Bhardwaj <shubham200015@yahoo.com>
2020-07-18 15:21:08 +0000

    Initial Commit with basic setup

---
+++ app.js
@@ -0,0 +1,20 @@
+var express 	= require("express"),
+	app 		= express(),
+	bodyParser	= require("body-parser");	
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", function(req,res){
+	res.render("landing");
+});
+
+app.get("/index", function(req,res){
+	res.send("All note groups will be available here!");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+	console.log("App has been started!");
+});
+	
